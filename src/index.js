b8bfc1f4b04b102687a063fb1aafe5a671419c8f
shireesha Bysegowni <shireeshareddy.b2002@gmail.com>
2023-11-29 18:38:27 +0530

    frontendcode

--- src/index.js
+++ src/index.js
@@ -1,40 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { store } from './redux/store';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-//Import necessary dependencies
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import {store} from './redux/store'; // Import your Redux store configuration
-// import App from './App';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
-import { createRoot } from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store'; // Import your Redux store configuration
+import { store } from './redux/store'; 
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
